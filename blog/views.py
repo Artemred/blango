50211a5bb931c7b99c2ffe31ec2e79d0a5e30182
Редін Артем <artemiyredin@gmail.com>
2024-06-21 12:51:18 +0000

    filters

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    return render(request, "index.html")
-# Create your views here.
+    posts = Post.objects.all()
+    return render(request, "index.html", {"posts": posts})
