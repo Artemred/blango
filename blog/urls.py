435ef867838e234da9e79e7012ac5a727730e091
Редін Артем <artemiyredin@gmail.com>
2024-06-22 07:54:04 +0000

    custom templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, post_detail
 
 app_name="blog"
 
 urlpatterns = [
-  path("", index, name="index")
+  path("", index, name="index"),
+      path("post/<slug>/", post_detail, name="blog-post-detail")
 ]
